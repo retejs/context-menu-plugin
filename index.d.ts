a8fc562c8d7c526041a521567a42c227a4f2ef8a
not-relevant-is-it <joel.edwardson1@gmail.com>
2021-11-10 12:18:54 +0000

    added types

---
+++ index.d.ts
@@ -0,0 +1,19 @@
+import { Plugin as RetePlugin } from 'rete/types/core/plugin';
+import { NodeEditor, Node, Component } from 'rete';
+import Vue from 'vue';
+
+export interface HistoryPlugin extends RetePlugin {
+  install: (editor: NodeEditor, options: {
+    searchBar?: boolean,
+    searchKeep?: (title: string) => boolean,
+    delay?: number,
+    items?: {[index: string]: () => any},
+    nodeItems?: {[index: string]: boolean | (() => void)} | ((node: Node) => {[index: string]: (() => void)}),
+    allocate?: (component: Component) => string[],
+    rename?: (component: Component) => string,
+    vueComponent?: typeof Vue.component,
+  }) => void,
+}
+
+declare const _default: HistoryPlugin;
+export default _default;
